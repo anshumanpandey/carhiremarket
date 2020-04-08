6e33f076f0782f1d17b4f03eb776f06a9b7a9397
leo <leo_9_t@hotmail.com>
2020-04-08 06:13:22 -0400

    FIX on CDW property

--- helpers/parser2.js
+++ helpers/parser2.js
@@ -32,7 +32,7 @@ module.exports.extractCars = function (script, q) {
       car.vehicle.price = `${offer.price.preferred.amount} ${offer.price.preferred.currency}`;
       car.vehicle.company = offer.vendor.name;
       car.vehicle.acriss = offer.car.vehicle.acriss;
-      car.vehicle.CDW = offer.info.reimbursedExcess;
+      car.vehicle.CDW = offer.info.cdw === null && offer.info.reimbursedExcess === true ? 0 : 1;
       cars.push(car);
     }
     let parsed = { scrape: { 'vehicles': cars, 'details': details } };
